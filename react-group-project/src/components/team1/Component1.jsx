52e3abb3f1c3078eb371bfb52a7f8a24a6dc3b8a
MaxwellConway <maxconway23@gmail.com>
2023-07-14 11:30:17 -0400

    added supabase client file

--- react-group-project/src/components/team1/Component1.jsx
+++ react-group-project/src/components/team1/Component1.jsx
@@ -1,5 +1,36 @@
+import { useEffect, useState } from "react";
+
+import supabase from "../../../../supabase";
+
 const Component1 = () => {
-  return <div>Component1</div>;
+  console.log("Hello");
+  const [esports, setEsports] = useState([]);
+
+  useEffect(() => {
+    const fetchEsportsData = async () => {
+      const { data, error } = await supabase.from("esports").select();
+      if (error) {
+        console.error("Error fetching esports:", error.message);
+      } else {
+        setEsports(data);
+      }
+    };
+
+    fetchEsportsData();
+  }, []);
+  console.log(esports);
+
+  return (
+    <>
+      <div>Component1</div>
+      <div>
+        {esports.map((esport) => (
+          <div key={esport.UUID}>{esport.username}</div>
+        ))}
+      </div>
+      <div>Component1</div>
+    </>
+  );
 };
 
 export default Component1;
