8a50a32a7e8334396325473132d3f80108b595e0
kimwells00 <wellskimberly22@gmail.com>
2023-07-14 14:45:46 -0400

    started tournament slice

--- react-group-project/src/store.js
+++ react-group-project/src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import tournamentReducer from "./reducers/tournamentSlice";
 // import fetchDatabaseData from "./supabaseSlice";
 export const store = configureStore({
   // reducers go here yall
   reducer: {
+    tournament: tournamentReducer,
     // [fetchDatabaseData.reducerPath]: fetchDatabaseData.reducer,
   },
 });
