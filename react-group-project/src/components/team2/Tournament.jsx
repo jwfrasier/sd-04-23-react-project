5c935e8f79855cebbf998ff8e9702e7e0d34b9c4
kimwells00 <wellskimberly22@gmail.com>
2023-07-17 12:04:52 -0400

    added tournament info to tournament component

--- react-group-project/src/components/team2/Tournament.jsx
+++ react-group-project/src/components/team2/Tournament.jsx
@@ -1,6 +1,29 @@
+/* eslint-disable react/prop-types */
+import "../../App.css";
 import React from "react";
 
+import { useState, useEffect } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+import supabaseConfig from "../../../../supabaseConfig";
+
 const Tournament = ({ tournamentImages }) => {
+  const [tournaments, setTournaments] = useState([]);
+  const { supabaseUrl, supabaseKey } = supabaseConfig;
+
+  const supabase = createClient(supabaseUrl, supabaseKey);
+  useEffect(() => {
+    const fetchEsportsTournamentsData = async () => {
+      const { data, error } = await supabase
+        .from("esports-tournaments")
+        .select();
+
+      setTournaments(data);
+    };
+
+    fetchEsportsTournamentsData();
+  }, []);
+
   return (
     <div>
       {tournamentImages.map((images) => (
@@ -8,6 +31,15 @@ const Tournament = ({ tournamentImages }) => {
           <img src={images.image} alt="photo" />
         </div>
       ))}
+      <div>
+        <h1>React Solo Project</h1>
+        {/* Render the tournament data */}
+        {tournaments.map((tournament) => (
+          <div key={tournament.id}>
+            <h2>{tournament.tournaments_name}</h2>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
