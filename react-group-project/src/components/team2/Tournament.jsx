d91c809fd0c8d4583d4739f1f05a05d9cf4a758a
Dustin Beckford <dustin.beckford@gmail.com>
2023-07-17 12:16:26 -0400

    Update Tournament.jsx
    updated css

--- react-group-project/src/components/team2/Tournament.jsx
+++ react-group-project/src/components/team2/Tournament.jsx
@@ -1,4 +1,9 @@
-/* eslint-disable react/prop-types */
+/**
+ * eslint-disable react/prop-types
+ *
+ * @format
+ */
+
 import "../../App.css";
 import React from "react";
 
@@ -8,40 +13,37 @@ import { createClient } from "@supabase/supabase-js";
 import supabaseConfig from "../../../../supabaseConfig";
 
 const Tournament = ({ tournamentImages }) => {
-  const [tournaments, setTournaments] = useState([]);
-  const { supabaseUrl, supabaseKey } = supabaseConfig;
+	const [tournaments, setTournaments] = useState([]);
+	const { supabaseUrl, supabaseKey } = supabaseConfig;
 
-  const supabase = createClient(supabaseUrl, supabaseKey);
-  useEffect(() => {
-    const fetchEsportsTournamentsData = async () => {
-      const { data, error } = await supabase
-        .from("esports-tournaments")
-        .select();
+	const supabase = createClient(supabaseUrl, supabaseKey);
+	useEffect(() => {
+		const fetchEsportsTournamentsData = async () => {
+			const { data, error } = await supabase
+				.from("esports-tournaments")
+				.select();
 
-      setTournaments(data);
-    };
+			setTournaments(data);
+		};
 
-    fetchEsportsTournamentsData();
-  }, []);
+		fetchEsportsTournamentsData();
+	}, []);
 
-  return (
-    <div>
-      {tournamentImages.map((images) => (
-        <div className="product-card" key={images.image}>
-          <img src={images.image} alt="photo" />
-        </div>
-      ))}
-      <div>
-        <h1>React Solo Project</h1>
-        {/* Render the tournament data */}
-        {tournaments.map((tournament) => (
-          <div key={tournament.id}>
-            <h2>{tournament.tournaments_name}</h2>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<h1>React Solo Project</h1>
+			{/* Render the tournament data */}
+			{tournamentImages.map((images, index) => (
+				<div className='product-card' key={images.image}>
+					<img src={images.image} alt='photo' />
+					{/* Render the tournament data for the corresponding image */}
+					<div key={tournaments[index].id}>
+						<h2>{tournaments[index].tournaments_name}</h2>
+					</div>
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default Tournament;
